c51576e6f6a6bee0092b2c6da5f464cc54bb8c35
Elijah King <elijahking@Elijahs-MBP.lusfiber.net>
2018-12-27 11:42:10 -0600

    I love this script.

--- personal-cv/auto.sh
+++ personal-cv/auto.sh
@@ -6,5 +6,6 @@ zip -r personal-cv.zip ./*
 docker stop $1
 docker rm $1
 docker build -t alecazzam/profile-deux:$2 ..
+docker push alecazzam/profile-deux:$2
 docker run -itd --name $1 --publish 8080:80 alecazzam/profile-deux:$2 
 exit 0
